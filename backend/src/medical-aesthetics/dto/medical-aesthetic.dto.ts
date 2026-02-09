297ffa033327750649be46536f7d1007d87db22a
yuxin <tiantony53@163.com>
2026-02-09 15:43:16 +0800

    修改dto

--- backend/src/medical-aesthetics/dto/medical-aesthetic.dto.ts
+++ backend/src/medical-aesthetics/dto/medical-aesthetic.dto.ts
@@ -3,7 +3,7 @@ import z from 'zod';
 
 const CreateMedicalAesthetiShema = z.object({
   category: z
-    .enum(['skin', 'face', 'eyes', 'nose', 'lips', 'other'])
+    .enum(['skin', 'face', 'eyes', 'nose', 'lips', 'poster', 'other'])
     .describe('选择添加提示词部位'),
   label: z.string().min(1, { message: '需要填写选项' }),
   prompt: z.string().min(1, { message: '需要填写提示词' }),
