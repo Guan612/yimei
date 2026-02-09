61e992904affb25be49506af7bcbd79c217ba183
yuxin <tiantony53@163.com>
2026-02-09 13:15:21 +0800

    删除无用组件

--- frontend/src/hooks/facesim/index.ts
+++ frontend/src/hooks/facesim/index.ts
@@ -1,12 +1,6 @@
 export { useCamera } from "./useCamera";
 export type { UseCameraReturn } from "./useCamera";
 
-export { usePhotoCapture } from "./usePhotoCapture";
-export type {
-  UsePhotoCaptureOptions,
-  UsePhotoCaptureReturn,
-} from "./usePhotoCapture";
-
 export { useFileUpload } from "./useFileUpload";
 export type {
   UseFileUploadOptions,
