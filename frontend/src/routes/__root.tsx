f28d1d5c95901c6ef712eddf890a407b2d9fc4ef
yuxin <tiantony53@163.com>
2026-02-10 10:30:36 +0800

    新增devtools

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,11 +1,12 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { Toaster } from "sonner";
-
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 export const Route = createRootRoute({
   component: () => (
     <>
       <Toaster />
       <Outlet />
+      <TanStackRouterDevtools />
     </>
   ),
 });
