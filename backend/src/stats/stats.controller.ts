38dd6a6654840ee6cf785463fe28d2b7ac80b082
yuxin <tiantony53@163.com>
2026-02-13 14:47:43 +0800

    优化staus

--- backend/src/stats/stats.controller.ts
+++ backend/src/stats/stats.controller.ts
@@ -2,10 +2,10 @@ import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { StatsService } from './stats.service';
 import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
 import { RolesGuard } from '../auth/guard/roles.guard';
-import { Roles } from '../auth/decorators/roles.decorator';
+import { Role, Roles } from '../auth/decorators/roles.decorator';
 import { GetStatsQueryDto } from './dto/stats.dto';
 
-@Controller('api/stats')
+@Controller('/stats')
 @UseGuards(JwtAuthGuard, RolesGuard)
 export class StatsController {
   constructor(private readonly statsService: StatsService) {}
@@ -14,7 +14,7 @@ export class StatsController {
    * 获取统计数据（管理员）
    */
   @Get()
-  @Roles(1) // 管理员角色
+  @Roles(Role.ADMIN) // 管理员角色
   async getStats(@Query() query: GetStatsQueryDto) {
     const data = await this.statsService.getStats(query);
     return {
